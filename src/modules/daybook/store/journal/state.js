581b29c23a820b87ab28f2389faad56dbaa185db
diego-garro <diego.garromolina@yahoo.com>
2021-10-16 22:41:35 -0600

    Clase 166: mapGetters - getEntriesByTerm

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,26 @@
 export default () => ({
   isLoading: true,
-  entries: [1, 2, 3, 4, 5],
+  entries: [
+    {
+      id: new Date().getTime(), // 12345875966325
+      date: new Date().toDateString(), // sat, 23, julio
+      text:
+        "Ipsum veniam ipsum minim laborum fugiat officia laborum quis sunt id qui sunt Lorem ipsum.",
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 1000, // 12345875966325
+      date: new Date().toDateString(), // sat, 23, julio
+      text:
+        "Veniam proident reprehenderit amet anim incididunt ullamco esse ipsum cupidatat commodo nisi ipsum officia aliquip.",
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 3000, // 12345875966325
+      date: new Date().toDateString(), // sat, 23, julio
+      text:
+        "Eu et irure do consectetur ut deserunt fugiat aliqua ea ex esse incididunt nisi.",
+      picture: null,
+    },
+  ],
 })
