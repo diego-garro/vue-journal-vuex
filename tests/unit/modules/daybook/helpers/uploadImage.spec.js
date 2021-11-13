8f5658d8ed9f5fef1756e27e8e4855f1ebd59bfa
diego-garro <diego.garromolina@yahoo.com>
2021-11-13 09:39:41 -0600

    Clase 202: Borrar archivos subidos en las pruebas

--- tests/unit/modules/daybook/helpers/uploadImage.spec.js
+++ tests/unit/modules/daybook/helpers/uploadImage.spec.js
@@ -1,8 +1,16 @@
-import uploadImage from "@/modules/daybook/helpers/uploadImage.js"
+import cloudinary from "cloudinary"
 import axios from "axios"
 
+import uploadImage from "@/modules/daybook/helpers/uploadImage.js"
+
+cloudinary.config({
+  cloud_name: "diebuntu",
+  api_key: "724221229563131",
+  api_secret: "moCOrpct2uIItSmNkYxyiqZNJl0",
+})
+
 describe("Pruebas en el uploadImage", () => {
-  test("debe de cargar un archivo y retornar el url", async () => {
+  test("debe de cargar un archivo y retornar el url", async (done) => {
     const { data } = await axios.get(
       "https://res.cloudinary.com/diebuntu/image/upload/v1636502343/l2nfoau7vy91raddkv5i.png",
       {
@@ -13,5 +21,12 @@ describe("Pruebas en el uploadImage", () => {
     const url = await uploadImage(file)
 
     expect(typeof url).toBe("string")
+
+    // Tomar el ID
+    const segments = url.split("/")
+    const imageId = segments[segments.length - 1].replace(".jpg", "")
+    cloudinary.v2.api.delete_resources(imageId, {}, () => {
+      done()
+    })
   })
 })
