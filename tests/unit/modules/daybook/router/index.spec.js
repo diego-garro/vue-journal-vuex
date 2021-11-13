a350040796d8c54a921636ae527e6b81410fbfc1
diego-garro <diego.garromolina@yahoo.com>
2021-11-13 08:34:52 -0600

    Clase 199: Comprobar que las rutas carguen los componentes esperados

--- tests/unit/modules/daybook/router/index.spec.js
+++ tests/unit/modules/daybook/router/index.spec.js
@@ -1,7 +1,7 @@
 import daybookRouter from "@/modules/daybook/router/index.js"
 
 describe("Pruebas en el router module del daybook", () => {
-  test("el router debe tener esta configuración", () => {
+  test("el router debe tener esta configuración", async () => {
     expect(daybookRouter).toMatchObject({
       name: "daybook",
       component: expect.any(Function),
@@ -19,5 +19,19 @@ describe("Pruebas en el router module del daybook", () => {
         },
       ],
     })
+    // expect((await daybookRouter.children[0].component()).default.name).toBe(
+    //   "NoEntrySelected"
+    // )
+    // expect((await daybookRouter.children[1].component()).default.name).toBe(
+    //   "EntryView"
+    // )
+    const promiseRoutes = []
+    daybookRouter.children.forEach((child) =>
+      promiseRoutes.push(child.component())
+    )
+
+    const routes = (await Promise.all(promiseRoutes)).map((r) => r.default.name)
+    expect(routes).toContain("EntryView")
+    expect(routes).toContain("NoEntrySelected")
   })
 })
